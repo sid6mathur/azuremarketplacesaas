6c7dffe88574fd255ec6fc5e3fb2d2dd584ad9a0
Siddharth Mathur <siddharth@getfastah.com>
2025-07-27 16:17:24 +0530

    Promote WellKnownScopeSaaSMetering as an exported const

--- metering/metering.go
+++ metering/metering.go
@@ -8,7 +8,7 @@ import (
 
 // The well-known metering service tenant ID for SaaS and Azure Applications with managed application plan type is 20e940b3-4c77-4b0b-9a53-9e16a1b010a7.
 // See https://learn.microsoft.com/en-us/partner-center/marketplace-offers/marketplace-metering-service-authentication#using-the-microsoft-entra-security-token
-const marketplaceSaaSMeteringScope = "20e940b3-4c77-4b0b-9a53-9e16a1b010a7/.default"
+const WellKnownScopeSaaSMetering = "20e940b3-4c77-4b0b-9a53-9e16a1b010a7/.default"
 
 // API endpoint for Azure Marketplace SaaS publisher operations
 const endpoint = "https://marketplaceapi.microsoft.com/api"
@@ -18,7 +18,7 @@ const endpoint = "https://marketplaceapi.microsoft.com/api"
 // options - pass nil to accept the default values.
 func NewMeteringClient(credential azcore.TokenCredential, options *policy.ClientOptions) (*OperationsClient, error) {
 	popts := runtime.PipelineOptions{
-		PerCall: []policy.Policy{runtime.NewBearerTokenPolicy(credential, []string{marketplaceSaaSMeteringScope}, nil)}}
+		PerCall: []policy.Policy{runtime.NewBearerTokenPolicy(credential, []string{WellKnownScopeSaaSMetering}, nil)}}
 	cl, err := azcore.NewClient(moduleName, moduleVersion, popts, options)
 	if err != nil {
 		return nil, err
